085718b525d6da8a69e878c112bf3984cdf78072
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-30 11:03:05 +0530

    [Arka]Added:Test to check count of employees based on gender

--- src/test/java/com/bl/db/EmployeePayrollServiceTest.java
+++ src/test/java/com/bl/db/EmployeePayrollServiceTest.java
@@ -11,27 +11,42 @@ import static org.junit.Assert.*;
 public class EmployeePayrollServiceTest {
     @Test
     public void readData() throws SQLException {
-        EmployeePayrollService employeePayrollService= new EmployeePayrollService();
-        List<EmployeePayrollData> employeePayrollDataList=employeePayrollService.readEmployeePayrollData();
-        Assert.assertEquals(4,employeePayrollDataList.size());
-       // System.out.println(employeePayrollDataList.size());
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollDataList = employeePayrollService.readEmployeePayrollData();
+        Assert.assertEquals(4, employeePayrollDataList.size());
+        // System.out.println(employeePayrollDataList.size());
 
     }
+
     @Test
     public void givenNewAddressForEmployee_WhenUpdated_ShouldMatch() throws SQLException {
-        EmployeePayrollService employeePayrollService=new EmployeePayrollService();
-        List<EmployeePayrollData> employeePayrollDataList= employeePayrollService.readEmployeePayrollData();
-        employeePayrollService.updateEmployeeAddress("Charlie","Kalighat");
-       boolean result=employeePayrollService.checkEmployeePayrollSyncWithDB("Charlie");
-       Assert.assertTrue(result);
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollDataList = employeePayrollService.readEmployeePayrollData();
+        employeePayrollService.updateEmployeeAddress("Charlie", "Kalighat");
+        boolean result = employeePayrollService.checkEmployeePayrollSyncWithDB("Charlie");
+        Assert.assertTrue(result);
 
 
     }
+
     @Test
     public void readDataWithinDate() throws SQLException {
-        EmployeePayrollService employeePayrollService= new EmployeePayrollService();
-        List<EmployeePayrollData> employeePayrollDataList=employeePayrollService.readEmployeePayrollDataWithinDate();
-        Assert.assertEquals(3,employeePayrollDataList.size());
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollDataList = employeePayrollService.readEmployeePayrollDataWithinDate();
+        Assert.assertEquals(3, employeePayrollDataList.size());
+    }
+
+    @Test
+    public void getEmployeeCount() throws SQLException {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        int countFemale = 0;
+        countFemale = employeePayrollService.getCountOfEmployees("F");
+        int countMale = 0;
+        countMale = employeePayrollService.getCountOfEmployees("F");
+        Assert.assertEquals(2, countFemale);
+        Assert.assertEquals(2, countMale);
+
+
     }
 
 }
