93df895bade45dffca54f4d01ffbf0ef23812a25
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-30 11:51:20 +0530

    [Arka]Added:Test to get sum,min and max of employees based on gender

--- src/test/java/com/bl/db/EmployeePayrollServiceTest.java
+++ src/test/java/com/bl/db/EmployeePayrollServiceTest.java
@@ -49,4 +49,39 @@ public class EmployeePayrollServiceTest {
 
     }
 
+    @Test
+    public void getEmployeeAvgSalary() throws SQLException {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        int avg = 0;
+        avg=employeePayrollService.getAvgSalaryOfEmployees("F");
+        int avg1 = 0;
+        avg1=employeePayrollService.getAvgSalaryOfEmployees("M");
+        Assert.assertEquals(102, avg);
+        Assert.assertEquals(102, avg1);
+
+    }
+    @Test
+    public void getEmployeeMaxMinSum() throws SQLException {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        int minFemale= 0;
+        minFemale = employeePayrollService.getMaxMinSalaryOfEmployees("F",EmployeePayrollDBService.getMin);
+        Assert.assertEquals(101, minFemale);
+        int maxFemale= 0;
+        maxFemale = employeePayrollService.getMaxMinSalaryOfEmployees("F",EmployeePayrollDBService.getMax);
+        Assert.assertEquals(103, maxFemale);
+        int maxMale= 0;
+        maxMale = employeePayrollService.getMaxMinSalaryOfEmployees("M",EmployeePayrollDBService.getMax);
+        Assert.assertEquals(102, maxMale);
+        int minMale= 0;
+        minMale = employeePayrollService.getMaxMinSalaryOfEmployees("M",EmployeePayrollDBService.getMin);
+        Assert.assertEquals(102, minMale);
+        int sumMale= 0;
+        sumMale = employeePayrollService.getMaxMinSalaryOfEmployees("M",EmployeePayrollDBService.getSum);
+        Assert.assertEquals(204, sumMale);
+        int sumFemale= 0;
+        sumFemale = employeePayrollService.getMaxMinSalaryOfEmployees("F",EmployeePayrollDBService.getSum);
+        Assert.assertEquals(204, sumFemale);
+
+    }
+
 }
